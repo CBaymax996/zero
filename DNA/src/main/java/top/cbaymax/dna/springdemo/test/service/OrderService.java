6cdc82a44434512528165ac9a08fb95835d20592
褚浩 <941174089@qq.com>
2023-11-23 12:47:14 +0800

    [spring learn] bean definition

---
+++ DNA/src/main/java/top/cbaymax/dna/springdemo/test/service/OrderService.java
@@ -0,0 +1,12 @@
+package top.cbaymax.dna.springdemo.test.service;
+
+import top.cbaymax.dna.springdemo.spring.annotation.Component;
+import top.cbaymax.dna.springdemo.spring.annotation.Scope;
+
+/**
+ * @author 褚浩
+ */
+@Component("orderService")
+@Scope
+public class OrderService {
+}
--- DNA/src/main/java/top/cbaymax/dna/springdemo/test/service/UserService.java
+++ DNA/src/main/java/top/cbaymax/dna/springdemo/test/service/UserService.java
@@ -1,5 +1,6 @@
 package top.cbaymax.dna.springdemo.test.service;
 
+import top.cbaymax.dna.springdemo.spring.annotation.Autowired;
 import top.cbaymax.dna.springdemo.spring.annotation.Component;
 import top.cbaymax.dna.springdemo.spring.annotation.Scope;
 import top.cbaymax.dna.springdemo.spring.annotation.ScopeType;
@@ -10,4 +11,11 @@ import top.cbaymax.dna.springdemo.spring.annotation.ScopeType;
 @Component("userService")
 @Scope(ScopeType.prototype)
 public class UserService {
+
+    @Autowired
+    private OrderService orderService;
+
+    public void testAutowired() {
+        System.out.println(orderService);
+    }
 }
